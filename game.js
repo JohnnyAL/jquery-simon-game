be6147f955322360e6629f63d88f72f8dcd711e6
Johnny Burke <johnalbertburke@gmail.com>
2021-04-13 17:38:04 -0700

    add start game logic

--- game.js
+++ game.js
@@ -2,6 +2,20 @@ let buttonColors = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
 let userPattern = [];
 
+//start the game
+let started = false;
+let level = 0;
+
+$(document).keydown(startGame);
+
+function startGame() {
+  if (!started) {
+    nextSequence();
+    // $("h1").text(`Level ${level}`);
+    started = true;
+  }
+}
+
 //next sequence
 function nextSequence() {
   let randomNum = Math.floor(Math.random() * 4);
@@ -11,6 +25,9 @@ function nextSequence() {
   $(`#${randomColor}`).fadeOut(100).fadeIn(100);
 
   playSound(randomColor);
+
+  level += 1;
+  $("h1").text(`Level ${level}`);
 }
 
 //click event
@@ -22,10 +39,20 @@ function handleClick() {
   console.log(userPattern);
 
   playSound(userChosenColor);
+  animatePress(userChosenColor);
 }
 
 //play sound
 function playSound(name) {
   let sound = new Audio(`sounds/${name}.mp3`);
   sound.play();
+}
+
+//animate button click
+function animatePress(color) {
+  $(`#${color}`).addClass("pressed");
+
+  setTimeout(function () {
+    $(`#${color}`).removeClass("pressed");
+  }, 100);
 }
